f77195a872835b9449be59715adcac486d8c1e2e
taylla.silva <taylla.silva@useall.com.br>
2023-07-05 18:16:09 -0300

    varEconst

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -5,10 +5,12 @@ describe('tarefas', ()=> {
 
     it('deve cadastrar uma nova tarefa', () => {
 
+        const taskName = "Ler um livro de Node.js"
+
         cy.request({
             url: 'http://localhost:3333/helper/tasks',
             method: 'DELETE',
-            body: {name: 'Ler um livro de Node.js'}
+            body: {name: taskName}
         }).then(response => {
             expect(response.status).to.eq(204)
         })
@@ -17,21 +19,56 @@ describe('tarefas', ()=> {
         cy.visit('http://localhost:8080')
 
         cy.get('input[placeholder="Add a new Task"]')
-         .type('Ler um livro de Node.js')     
+         .type(taskName)     
 
         cy.contains('button', 'Create').click()
 
-        // cy.get('main div p')
-        //     .should('be.visible')
-        //     .should('have.text', 'Ler um livro de Node.js')
-
-        cy.contains('main div p','Ler um livro de Node.js')
+        cy.contains('main div p',taskName)
         .should('be.visible')
 
     }) 
 
     it('Não deve permitir tarefa duplicada', () => {
-        
+
+        const task = {
+            name: 'Estudar Javascript',
+            is_done: false
+        }
+
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: {name: task.name}
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })
+
+        // Dado que eu tenho uma terfa duplicada 
+
+        cy.request({
+            url: 'http://localhost:3333/tasks',
+            method: 'POST',
+            body: task
+
+        }).then(response => {
+            expect(response.status).to.eq(201)
+        })
+
+
+        // Quano faço o cadastro dessa tarefa 
+
+        cy.visit('http://localhost:8080')
+
+        cy.get('input[placeholder="Add a new Task"]')
+         .type(task.name)     
+
+        cy.contains('button', 'Create').click()
+
+        // Então vejo a mensagem de duplicidade  
+        cy.get('.swal2-html-container')
+        .should('be.visible')
+        .should('have.text','Task already exists!')
+
     });
 
 })
