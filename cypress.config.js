d8a306282aa5c36cdec74523e0727ebbc4884dee
taylla.silva <taylla.silva@useall.com.br>
2023-07-06 14:17:43 -0300

    finalizado

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,16 @@
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
 module.exports = {
   e2e: {
+    baseUrl: 'http://localhost:8080',
+    env: {
+      apiUrl: 'http://localhost:3333'
+    },
+    viewportWidch: 1920,
+    viewportHeight: 1080,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
   },
 };
--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,76 +1,87 @@
-import {faker} from '@faker-js/faker'
+describe('tarefas', () => {
 
-describe('tarefas', ()=> {
+    let testeData;
 
-
-    it('deve cadastrar uma nova tarefa', () => {
-
-        const taskName = "Ler um livro de Node.js"
-
-        cy.request({
-            url: 'http://localhost:3333/helper/tasks',
-            method: 'DELETE',
-            body: {name: taskName}
-        }).then(response => {
-            expect(response.status).to.eq(204)
+    before(() => {
+        cy.fixture('tasks').then(t => {
+            testeData = t
         })
-      
-      
-        cy.visit('http://localhost:8080')
+    })
 
-        cy.get('input[placeholder="Add a new Task"]')
-         .type(taskName)     
+    context('Cadastro', () => {
+        it('Deve cadastrar uma nova tarefa', () => {
+            const taskName = "Ler um livro de Node.js"
 
-        cy.contains('button', 'Create').click()
+            cy.removeTaskByName(taskName)
+            cy.createTask(taskName)
 
-        cy.contains('main div p',taskName)
-        .should('be.visible')
-
-    }) 
-
-    it('Não deve permitir tarefa duplicada', () => {
-
-        const task = {
-            name: 'Estudar Javascript',
-            is_done: false
-        }
-
-        cy.request({
-            url: 'http://localhost:3333/helper/tasks',
-            method: 'DELETE',
-            body: {name: task.name}
-        }).then(response => {
-            expect(response.status).to.eq(204)
+            cy.contains('main div p', taskName)
+                .should('be.visible')
         })
 
-        // Dado que eu tenho uma terfa duplicada 
+        it('Não deve permitir tarefa duplicada', () => {
+            const task = testeData.dup
 
-        cy.request({
-            url: 'http://localhost:3333/tasks',
-            method: 'POST',
-            body: task
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+            cy.createTask(task.name)
 
-        }).then(response => {
-            expect(response.status).to.eq(201)
+            cy.get('.swal2-html-container')
+                .should('be.visible')
+                .should('have.text', 'Task already exists!')
+        });
+
+        it('Campo obrigatório', () => {
+            cy.createTask()
+            cy.isrequired('This is a required field')
         })
+    })
 
+    context('Atualização', () => {
+        it('Deve concluir uma tarefa', () => {
+            const task = {
+                name: 'pagar contas de consumo',
+                is_done: false
+            }
 
-        // Quano faço o cadastro dessa tarefa 
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
 
-        cy.visit('http://localhost:8080')
+            cy.visit('/')
 
-        cy.get('input[placeholder="Add a new Task"]')
-         .type(task.name)     
+            cy.contains('p', task.name)
+                .parent()
+                .find('button[class*=ItemToggle]')
+                .click()
 
-        cy.contains('button', 'Create').click()
+            cy.contains('p', task.name)
+                .should('have.css', 'text-decoration-line', 'line-through')
 
-        // Então vejo a mensagem de duplicidade  
-        cy.get('.swal2-html-container')
-        .should('be.visible')
-        .should('have.text','Task already exists!')
+        })
+    })
 
-    });
+    context('Exclusão', () => {
+        it('Deve remover uma tarefa', () => {
+            const task = {
+                name: 'Estudar Javascript',
+                is_done: false
+            }
 
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+
+            cy.visit('/')
+
+            cy.contains('p', task.name)
+                .parent()
+                .find('button[class*=ItemDelete]')
+                .click()
+
+            cy.contains('p', task.name)
+                .should('not.exist')
+
+        })
+    })
 })
 
 
